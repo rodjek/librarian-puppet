30a74cff7c8e157eccb5f3a2e4bc5a8e3648937a
Tim Sharpe <tim@sharpe.id.au>
2012-06-21 12:59:15 +1000

    Always fetch the commit SHA from rev-parse
    Nasty monkey patch of Librarian::Source::Git::Repository so when dealing with
    things like annotated tags, we look up the commit SHA of a reference rather
    than an object SHA.
    
    Closes #12

--- lib/librarian/puppet/source/git.rb
+++ lib/librarian/puppet/source/git.rb
@@ -2,6 +2,22 @@ require 'librarian/source/git'
 require 'librarian/puppet/source/local'
 
 module Librarian
+  module Source
+    class Git
+      class Repository
+        def hash_from(remote, reference)
+          branch_names = remote_branch_names[remote]
+          if branch_names.include?(reference)
+            reference = "#{remote}/#{reference}"
+          end
+
+          command = %W(rev-parse #{reference}^{commit} --quiet)
+          run!(command, :chdir => true).strip
+        end
+      end
+    end
+  end
+
   module Puppet
     module Source
       class Git < Librarian::Source::Git
