5bcaa8d85ea989c07278e55463c0c971a41879e8
Will Farrington <wfarr@github.com>
2013-02-13 15:02:11 -0800

    try to coerce semver tags into something we can work with

--- lib/librarian/puppet/source/githubtarball.rb
+++ lib/librarian/puppet/source/githubtarball.rb
@@ -23,7 +23,16 @@ module Librarian
             end
 
             all_versions = data.map { |r| r['name'] }.sort.reverse
-            all_versions.reject { |version| version =~ /^v/ }.compact
+
+            all_versions = all_versions.map do |version|
+              version.gsub(/^v/, '')
+            end
+
+            all_versions.delete_if do |version|
+              version !~ /\A\d\.\d(\.\d.*)?\z/
+            end
+
+            all_versions.compact
           end
 
           def manifests
