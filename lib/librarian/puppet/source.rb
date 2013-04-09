ec12fd25894b0ac4112eb4893795901852eca5af
Eduardo Gutierrez <edd_d@mit.edu>
2013-04-09 19:08:40 -0400

    Convert puppet version requirements
    Checks puppet requirement to see if it is pessimistic (1.2.x) or a range
    (>=3.0.0 <4.0.0) and converts them to their respective rubygems
    requirement (~> 1.2 and ['>= 3.0.0', '<4.0.0'] respectively).

--- lib/librarian/puppet/source.rb
+++ lib/librarian/puppet/source.rb
@@ -1,3 +1,4 @@
+require 'librarian/puppet/requirement'
 require 'librarian/puppet/source/path'
 require 'librarian/puppet/source/git'
 require 'librarian/puppet/source/forge'
--- lib/librarian/puppet/source/forge.rb
+++ lib/librarian/puppet/source/forge.rb
@@ -265,6 +265,7 @@ module Librarian
 
         def fetch_dependencies(name, version, version_uri)
           repo(name).dependencies(version).map do |k, v|
+            v = Requirement.new(v).gem_requirement
             Dependency.new(k, v, nil)
           end
         end
