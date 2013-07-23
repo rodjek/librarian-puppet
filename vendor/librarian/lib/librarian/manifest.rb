2961bd5d9208362bb4021b39703e383111c3a13d
Justen Walker <justen.walker+github@gmail.com>
2013-07-23 14:57:26 -0400

    Make Librarian::Manifest Semver 2.0.0 compatible
    Semver metadata cannot be parsed by Gem::Version and causes
    and exception at runtime on modules which include it.
    
    This metadata is part of Semver 2.0.0 (http://semver.org/spec/v2.0.0.html)
    
    - Added Semver 2.0.0 support to Librarian::Manifest::Version
    - Relaxed constraints to support 2-number versions (ie: 1.0)
    - Added specs to test conditions mentioned in semver spec

--- vendor/librarian/lib/librarian/manifest.rb
+++ vendor/librarian/lib/librarian/manifest.rb
@@ -4,12 +4,59 @@ module Librarian
   class Manifest
 
     class Version
+      @@SEMANTIC_VERSION_PATTERN = /^([0-9]+\.[0-9]+(?:\.[0-9]+)?)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?$/
+      def self.parse(version_string)
+        parsed = @@SEMANTIC_VERSION_PATTERN.match(version_string.strip)
+        raise ArgumentError, "Invalid Semantic Version String '#{version_string}'" unless parsed
+        {
+            :full_version   => parsed[0],
+            :version        => parsed[1],
+            :prerelease     => parsed[2],
+            :build          => parsed[3],
+            :prerelease_ids => self.parse_metadata(parsed[2])
+        }
+      end
+
+      def self.parse_metadata(metadata)
+        if metadata.nil?
+          []
+        else
+          metadata.split('.').collect do |id|
+            id = Integer(id) if /^[0-9]+$/ =~ id
+            id
+          end
+        end
+      end
+
+      # Compare Pre-release Ids
+      def self.compare_ids(id1,id2)
+        if id1.is_a?(Integer) or id2.is_a?(Integer)
+          # Numeric ids have lower precedence than alpha ids
+          if id1.is_a?(String)
+            return 1
+          elsif id2.is_a?(String)
+            return -1
+          end
+        end
+        id1 <=> id2
+      end
+
       include Comparable
 
-      def initialize(*args)
-        args = initialize_normalize_args(args)
+      attr_reader :prerelease_ids
+      attr_reader :prerelease
+      attr_reader :full_version
 
-        self.backing = Gem::Version.new(*args)
+      def initialize(version)
+        semver = Version.parse(version)
+        self.backing    = Gem::Version.new(semver[:version])
+        @full_version   = semver[:full_version]
+        @prerelease     = semver[:prerelease]
+        @prerelease_ids = semver[:prerelease_ids]
+      end
+
+      def has_prerelease?
+        not @prerelease.nil?
       end
 
       def to_gem_version
@@ -17,21 +64,39 @@ module Librarian
       end
 
       def <=>(other)
-        to_gem_version <=> other.to_gem_version
+        version_compare = to_gem_version <=> other.to_gem_version
+        if version_compare == 0 and (has_prerelease? or other.has_prerelease?)
+          # Versions without a prerelease version win over those which have one
+          if not has_prerelease? and other.has_prerelease?  # My version wins
+            1
+          elsif not other.has_prerelease? and has_prerelease? # Their version wins
+            -1
+          else # Both have pre-release versions; comparison is mandatory
+            z = Array.new([prerelease_ids.length,other.prerelease_ids.length].max)
+            (z.zip(prerelease_ids,other.prerelease_ids).collect { |ids|
+              if ids[1].nil? # self has less elements, other wins
+                -1
+              elsif ids[2].nil? # other has less elements, self wins
+                1
+              else
+                Version.compare_ids(ids[1],ids[2]) # Normal comparison of ids
+              end
+            }).delete_if {|c| c == 0}[0] || 0
+          end
+        else
+          version_compare
+        end
       end
 
       def to_s
-        to_gem_version.to_s
+        if has_prerelease?
+          "#{to_gem_version.to_s}-#{@prerelease}"
+        else
+          to_gem_version.to_s
+        end
       end
 
       private
-
-      def initialize_normalize_args(args)
-        args.map do |arg|
-          arg = [arg] if self.class === arg
-          arg
-        end
-      end
 
       attr_accessor :backing
     end
@@ -89,7 +154,7 @@ module Librarian
       source.install!(self)
     end
 
-  private
+    private
 
     attr_accessor :defined_version, :defined_dependencies
 
