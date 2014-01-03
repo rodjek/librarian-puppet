1d9ab9aeaac0ec6fc7e14d3606a45a62ea4b07f0
Carlos Sanchez <csanchez@maestrodev.com>
2014-01-03 14:11:15 +0100

    Add open3_backport in ruby 1.8

--- lib/librarian/puppet.rb
+++ lib/librarian/puppet.rb
@@ -1,27 +1,32 @@
 require 'librarian'
 require 'fileutils'
 require 'open3'
+require 'open3_backport' if RUBY_VERSION < '1.9'
 
 status = nil
-puppet_version = nil
+out = nil
+error = nil
 
 begin
   Open3.popen3('puppet --version') {|stdin, stdout, stderr, wait_thr|
     pid = wait_thr.pid # pid of the started process.
-    puppet_version = stdout.read
+    out = stdout.read
     status = wait_thr.value # Process::Status object returned.
   }
-rescue
+rescue => e
+  error = e
 end
 
 if status.nil? or status.exitstatus != 0
   $stderr.puts <<-EOF
 Unable to load puppet. Please install it using native packages for your platform (eg .deb, .rpm, .dmg, etc).
+#{out.nil? or out.empty? ? "puppet --version returned #{status.exitstatus}" : out}
+#{error.message unless error.nil?}
 EOF
   exit 1
 end
 
-PUPPET_VERSION=puppet_version.split(' ').first.strip
+PUPPET_VERSION=out.split(' ').first.strip
 
 require 'librarian/puppet/extension'
 require 'librarian/puppet/version'
