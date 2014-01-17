89ae7f2e479587a6bfc19457211af7d54f4857de
Carlos Sanchez <csanchez@maestrodev.com>
2014-01-17 11:40:37 +0100

    Issue #167 Require open3_backport in ruby 1.8 and install if not present

--- lib/librarian/puppet.rb
+++ lib/librarian/puppet.rb
@@ -1,7 +1,18 @@
 require 'librarian'
 require 'fileutils'
 require 'open3'
-require 'open3_backport' if RUBY_VERSION < '1.9'
+
+# require open3_backport in ruby 1.8 and install if not present
+# https://github.com/rodjek/librarian-puppet/issues/167
+if RUBY_VERSION < '1.9'
+  begin
+    require 'open3_backport'
+  rescue LoadError
+    require 'rubygems/dependency_installer.rb'
+    Gem::DependencyInstaller.new.install 'open3_backport'
+    require 'open3_backport'
+  end
+end
 
 status = nil
 out = nil
