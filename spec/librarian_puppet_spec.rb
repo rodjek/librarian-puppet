rodjek/librarian-puppet

commit 3b694955db500f23665d481b34a3e7d6878ea3d6

Use librarian posix library instead of open3
Handles jruby, ruby 1.8, 1.9+
Don't use backticks anywhere

lib/librarian/puppet.rb
@@ -1,43 +1,15 @@
 require 'librarian'
 require 'fileutils'
-require 'open3'
-require 'open3_backport' if RUBY_VERSION < '1.9'
 
-status = nil
 out = nil
-err = nil
-error = nil
-
 begin
-  if RUBY_VERSION < '1.9'
-    # Ruby 1.8.x backport of popen3 doesn't allow the 'env' hash argument
-    # Not sanitizing the environment for the moment.
-    Open3.popen3('puppet --version') { |stdin, stdout, stderr, wait_thr|
-      pid = wait_thr.pid # pid of the started process.
-      out = stdout.read
-      err = stderr.read
-      status = wait_thr.value # Process::Status object returned.
-    }
-  else
-    env_reset = {'BUNDLE_APP_CONFIG' => nil, 'BUNDLE_CONFIG' => nil, 'BUNDLE_GEMFILE' => nil, 'BUNDLE_BIN_PATH' => nil,
-                 'RUBYLIB' => nil, 'RUBYOPT' => nil, 'GEMRC' => nil, 'GEM_PATH' => nil}
-    Open3.popen3(env_reset, 'puppet --version') { |stdin, stdout, stderr, wait_thr|
-      pid = wait_thr.pid # pid of the started process.
-      out = stdout.read
-      err = stderr.read
-      status = wait_thr.value # Process::Status object returned.
-    }
-  end
-rescue => e
-  error = e
-end
+  out = Librarian::Posix.run!(%W{puppet --version})
+rescue Librarian::Posix::CommandFailure => error
 
-if status.nil? or status.exitstatus != 0
   $stderr.puts <<-EOF
 Unable to load puppet. Please install it using native packages for your platform (eg .deb, .rpm, .dmg, etc).
-#{out.nil? or out.empty? ? "puppet --version returned #{status.exitstatus}" : out}
-#{error.message unless error.nil?}
-#{err unless err.nil?}
+puppet --version returned #{status.exitstatus}
+#{error.message unless error.message.nil?}
 EOF
   exit 1
 end

lib/librarian/puppet/source/forge.rb
@@ -101,18 +101,19 @@ module Librarian
 
             path.mkpath
 
-            target = vendored?(name, version) ? vendored_path(name, version) : name
+            target = vendored?(name, version) ? vendored_path(name, version).to_s : name
 
 
-            command = "puppet module install --version #{version} --target-dir '#{path}' --module_repository '#{source}' --modulepath '#{path}' --module_working_dir '#{path}' --ignore-dependencies '#{target}'"
+            command = %W{puppet module install --version #{version} --target-dir}
+            command.push(*[path.to_s, "--module_repository", source.to_s, "--modulepath", path.to_s, "--module_working_dir", path.to_s, "--ignore-dependencies", target])
             debug { "Executing puppet module install for #{name} #{version}" }
-            output = `#{command}`
 
-            # Check for bad exit code
-            unless $? == 0
+            begin
+              Librarian::Posix.run!(command)
+            rescue Posix::CommandFailure => e
               # Rollback the directory if the puppet module had an error
               path.unlink
-              raise Error, "Error executing puppet module install:\n#{command}\nError:\n#{output}"
+              raise Error, "Error executing puppet module install:\n#{command.join(" ")}\nError:\n#{e.message}"
             end
 
           end

lib/librarian/puppet/source/git.rb
@@ -48,17 +48,13 @@ module Librarian
           repository.path.rmtree if repository.path.exist?
           repository.path.mkpath
 
-          Dir.chdir(repository.path.to_s) do
-            %x{tar xzf #{vendor_tgz}}
-          end
+          run!(%W{tar xzf #{vendor_tgz}}, :chdir => repository.path.to_s)
 
           repository_cached!
         end
 
         def cache_in_vendor(tmp_path)
-          Dir.chdir(tmp_path.to_s) do
-            %x{git archive #{sha} | gzip > #{vendor_tgz}}
-          end
+          run!(%W{git archive #{sha} | gzip > #{vendor_tgz}}, :chdir => tmp_path.to_s)
         end
 
       end

lib/librarian/puppet/source/githubtarball.rb
@@ -86,7 +86,7 @@ module Librarian
 
             target = vendored?(source.uri, version) ? vendored_path(source.uri, version) : name
 
-            `tar xzf #{target} -C #{path}`
+            Librarian::Posix.run!(%W{tar xzf #{target} -C #{path}})
           end
 
           def vendored?(name, version)

librarian-puppet.gemspec
@@ -27,7 +27,6 @@ Gem::Specification.new do |s|
 
   # only needed for ruby 1.8
   s.add_dependency "json"
-  s.add_dependency "open3_backport"
 
   s.add_development_dependency "rake"
   s.add_development_dependency "rspec"

commit 3e1fe89ffacee86d7404b6feb10bcef56b90c75a

Fix error handling when puppet is not installed
Bug introduced by 3b694955db500f23665d481b34a3e7d6878ea3d6. The error message
generated in response to an error in running 'puppet --version' itself
generated a new error by referencing a 'status' method that does not exist.

lib/librarian/puppet.rb
@@ -1,21 +1,6 @@
 require 'librarian'
 require 'fileutils'
 
-out = nil
-begin
-  out = Librarian::Posix.run!(%W{puppet --version})
-rescue Librarian::Posix::CommandFailure => error
-
-  $stderr.puts <<-EOF
-Unable to load puppet. Please install it using native packages for your platform (eg .deb, .rpm, .dmg, etc).
-puppet --version returned #{status.exitstatus}
-#{error.message unless error.message.nil?}
-EOF
-  exit 1
-end
-
-PUPPET_VERSION=out.split(' ').first.strip
-
 require 'librarian/puppet/extension'
 require 'librarian/puppet/version'
 
@@ -23,5 +8,22 @@ require 'librarian/action/install'
 
 module Librarian
   module Puppet
+    def puppet_version
+      out = nil
+      begin
+        out = Librarian::Posix.run!(%W{puppet --version})
+      rescue Librarian::Posix::CommandFailure => error
+
+        $stderr.puts <<-EOF
+      Unable to load puppet. Please install it using native packages for your platform (eg .deb, .rpm, .dmg, etc).
+      puppet --version returned #{error.status}
+      #{error.message unless error.message.nil?}
+      EOF
+        exit 1
+      end
+      out.split(' ').first.strip
+    end
   end
 end
+
+PUPPET_VERSION=Librarian::Puppet::puppet_version

spec/librarian_puppet_spec.rb
@@ -1,0 +1,14 @@
+describe Librarian::Puppet do
+  it 'exits with error if puppet is not installed' do
+    error = Librarian::Posix::CommandFailure.new 'puppet not installed'
+    error.status = 42
+
+    expect(Librarian::Posix).to receive(:run!).and_raise(error)
+    expect($stderr).to receive(:puts) do |message|
+      expect(message).to match /42/
+      expect(message).to match /puppet not installed/
+    end
+
+    expect { Librarian::Puppet::puppet_version }.to raise_error(SystemExit)
+  end
+end
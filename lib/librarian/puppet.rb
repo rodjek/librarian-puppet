3e1fe89ffacee86d7404b6feb10bcef56b90c75a
Phil Frost <pfrost@macprofessionals.com>
2014-04-08 13:25:09 -0400

    Fix error handling when puppet is not installed
    Bug introduced by 3b694955db500f23665d481b34a3e7d6878ea3d6. The error message
    generated in response to an error in running 'puppet --version' itself
    generated a new error by referencing a 'status' method that does not exist.

--- lib/librarian/puppet.rb
+++ lib/librarian/puppet.rb
@@ -1,21 +1,6 @@
 require 'librarian'
 require 'fileutils'
 
-out = nil
-begin
-  out = Librarian::Posix.run!(%W{puppet --version})
-rescue Librarian::Posix::CommandFailure => error
-
-  $stderr.puts <<-EOF
-Unable to load puppet. Please install it using native packages for your platform (eg .deb, .rpm, .dmg, etc).
-puppet --version returned #{status.exitstatus}
-#{error.message unless error.message.nil?}
-EOF
-  exit 1
-end
-
-PUPPET_VERSION=out.split(' ').first.strip
-
 require 'librarian/puppet/extension'
 require 'librarian/puppet/version'
 
@@ -23,5 +8,22 @@ require 'librarian/action/install'
 
 module Librarian
   module Puppet
+    def puppet_version
+      out = nil
+      begin
+        out = Librarian::Posix.run!(%W{puppet --version})
+      rescue Librarian::Posix::CommandFailure => error
+
+        $stderr.puts <<-EOF
+      Unable to load puppet. Please install it using native packages for your platform (eg .deb, .rpm, .dmg, etc).
+      puppet --version returned #{error.status}
+      #{error.message unless error.message.nil?}
+      EOF
+        exit 1
+      end
+      out.split(' ').first.strip
+    end
   end
 end
+
+PUPPET_VERSION=Librarian::Puppet::puppet_version
