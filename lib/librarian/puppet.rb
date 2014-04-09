e1dc2ebe45a73945bb9420cf3d853e226c4e8003
Carlos Sanchez <csanchez@maestrodev.com>
2014-04-09 18:44:21 +0200

    Add some comments

--- lib/librarian/puppet.rb
+++ lib/librarian/puppet.rb
@@ -10,6 +10,8 @@ module Librarian
   module Puppet
     @@puppet_version = nil
 
+    # Output of puppet --version, typically x.y.z
+    # For Puppet Enterprise it contains the PE version too, ie. 3.4.3 (Puppet Enterprise 3.2.1)
     def puppet_version
       return @@puppet_version unless @@puppet_version.nil?
 
@@ -25,6 +27,7 @@ module Librarian
       return @@puppet_version
     end
 
+    # Puppet version x.y.z translated as a Gem version
     def puppet_gem_version
       Gem::Version.create(puppet_version.split(' ').first.strip.gsub('-', '.'))
     end
