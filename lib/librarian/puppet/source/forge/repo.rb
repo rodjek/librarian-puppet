66ed19e381edf1811957b481eaad9856e3fdfc07
Carlos Sanchez <csanchez@maestrodev.com>
2014-05-10 18:13:25 +0200

    Move Repo classes out of forge and githubtarball sources

---
+++ lib/librarian/puppet/source/forge/repo.rb
@@ -0,0 +1,208 @@
+require 'json'
+require 'open-uri'
+require 'librarian/puppet/util'
+
+module Librarian
+  module Puppet
+    module Source
+      class Forge
+        class Repo
+          include Librarian::Puppet::Util
+
+          attr_accessor :source, :name
+          private :source=, :name=
+
+          def initialize(source, name)
+            self.source = source
+            self.name = name
+            # API returned data for this module including all versions and dependencies, indexed by module name
+            # from http://forge.puppetlabs.com/api/v1/releases.json?module=#{name}
+            @api_data = nil
+            # API returned data for this module and a specific version, indexed by version
+            # from http://forge.puppetlabs.com/api/v1/releases.json?module=#{name}&version=#{version}
+            @api_version_data = {}
+          end
+
+          def versions
+            return @versions if @versions
+            @versions = api_data(name).map { |r| r['version'] }.reverse
+            if @versions.empty?
+              info { "No versions found for module #{name}" }
+            else
+              debug { "  Module #{name} found versions: #{@versions.join(", ")}" }
+            end
+            @versions
+          end
+
+          def dependencies(version)
+            api_version_data(name, version)['dependencies']
+          end
+
+          def manifests
+            versions.map do |version|
+              Manifest.new(source, name, version)
+            end
+          end
+
+          def install_version!(version, install_path)
+            if environment.local? && !vendored?(name, version)
+              raise Error, "Could not find a local copy of #{name} at #{version}."
+            end
+
+            if environment.vendor?
+              vendor_cache(name, version) unless vendored?(name, version)
+            end
+
+            cache_version_unpacked! version
+
+            if install_path.exist?
+              install_path.rmtree
+            end
+
+            unpacked_path = version_unpacked_cache_path(version).join(name.split('/').last)
+
+            unless unpacked_path.exist?
+              raise Error, "#{unpacked_path} does not exist, something went wrong. Try removing it manually"
+            else
+              cp_r(unpacked_path, install_path)
+            end
+
+          end
+
+          def environment
+            source.environment
+          end
+
+          def cache_path
+            @cache_path ||= source.cache_path.join(name)
+          end
+
+          def version_unpacked_cache_path(version)
+            cache_path.join('version').join(hexdigest(version.to_s))
+          end
+
+          def hexdigest(value)
+            Digest::MD5.hexdigest(value)
+          end
+
+          def cache_version_unpacked!(version)
+            path = version_unpacked_cache_path(version)
+            return if path.directory?
+
+            # The puppet module command is only available from puppet versions >= 2.7.13
+            #
+            # Specifying the version in the gemspec would force people to upgrade puppet while it's still usable for git
+            # So we do some more clever checking
+            #
+            # Executing older versions or via puppet-module tool gives an exit status = 0 .
+            #
+            check_puppet_module_options
+
+            path.mkpath
+
+            target = vendored?(name, version) ? vendored_path(name, version).to_s : name
+
+            # TODO can't pass the default forge url (http://forge.puppetlabs.com) to clients that use the v3 API (https://forgeapi.puppetlabs.com)
+            module_repository = source.to_s
+            if Forge.client_api_version() > 1 and module_repository =~ %r{^http(s)?://forge\.puppetlabs\.com}
+              module_repository = "https://forgeapi.puppetlabs.com"
+            end
+
+            command = %W{puppet module install --version #{version} --target-dir}
+            command.push(*[path.to_s, "--module_repository", module_repository, "--modulepath", path.to_s, "--module_working_dir", path.to_s, "--ignore-dependencies", target])
+            debug { "Executing puppet module install for #{name} #{version}" }
+
+            begin
+              Librarian::Posix.run!(command)
+            rescue Posix::CommandFailure => e
+              # Rollback the directory if the puppet module had an error
+              begin
+                path.unlink
+              rescue => u
+                debug("Unable to rollback path #{path}: #{u}")
+              end
+              tar = Dir[File.join(path.to_s, "**/*.tar.gz")]
+              msg = ""
+              if e.message =~ /Unexpected EOF in archive/ and !tar.empty?
+                file = tar.first
+                msg = " (looks like an incomplete download of #{file})"
+              end
+              raise Error, "Error executing puppet module install#{msg}. Check that this command succeeds:\n#{command.join(" ")}\nError:\n#{e.message}"
+            end
+
+          end
+
+          def check_puppet_module_options
+            min_version    = Gem::Version.create('2.7.13')
+
+            if Librarian::Puppet.puppet_gem_version < min_version
+              raise Error, "To get modules from the forge, we use the puppet faces module command. For this you need at least puppet version 2.7.13 and you have #{puppet_version}"
+            end
+          end
+
+          def vendored?(name, version)
+            vendored_path(name, version).exist?
+          end
+
+          def vendored_path(name, version)
+            environment.vendor_cache.join("#{name.sub("/", "-")}-#{version}.tar.gz")
+          end
+
+          def vendor_cache(name, version)
+            info = api_version_data(name, version)
+            url = "#{source}#{info[name].first['file']}"
+            path = vendored_path(name, version).to_s
+            debug { "Downloading #{url} into #{path}"}
+            File.open(path, 'wb') do |f|
+              open(url, "rb") do |input|
+                f.write(input.read)
+              end
+            end
+          end
+
+        private
+
+          # get and cache the API data for a specific module with all its versions and dependencies
+          def api_data(module_name)
+            return @api_data[module_name] if @api_data
+            # call API and cache data
+            @api_data = api_call(module_name)
+            if @api_data.nil?
+              raise Error, "Unable to find module '#{name}' on #{source}"
+            end
+            @api_data[module_name]
+          end
+
+          # get and cache the API data for a specific module and version
+          def api_version_data(module_name, version)
+            # if we already got all the versions, find in cached data
+            return @api_data[module_name].detect{|x| x['version'] == version.to_s} if @api_data
+            # otherwise call the api for this version if not cached already
+            @api_version_data[version] = api_call(name, version) if @api_version_data[version].nil?
+            @api_version_data[version]
+          end
+
+          def api_call(module_name, version=nil)
+            base_url = source.uri
+            path = "api/v1/releases.json?module=#{module_name}"
+            path = "#{path}&version=#{version}" unless version.nil?
+            url = "#{base_url}/#{path}"
+            debug { "Querying Forge API for module #{name}#{" and version #{version}" unless version.nil?}: #{url}" }
+
+            begin
+              data = open(url) {|f| f.read}
+              JSON.parse(data)
+            rescue OpenURI::HTTPError => e
+              case e.io.status[0].to_i
+              when 404,410
+                nil
+              else
+                raise e, "Error requesting #{base_url}/#{path}: #{e.to_s}"
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/librarian/puppet/source/githubtarball/repo.rb
@@ -0,0 +1,180 @@
+require 'uri'
+require 'net/https'
+require 'open-uri'
+require 'json'
+
+require 'librarian/puppet/version'
+
+module Librarian
+  module Puppet
+    module Source
+      class GitHubTarball
+        class Repo
+          include Librarian::Puppet::Util
+
+          TOKEN_KEY = 'GITHUB_API_TOKEN'
+
+          attr_accessor :source, :name
+          private :source=, :name=
+
+          def initialize(source, name)
+            self.source = source
+            self.name = name
+          end
+
+          def versions
+            return @versions if @versions
+            data = api_call("/repos/#{source.uri}/tags")
+            if data.nil?
+              raise Error, "Unable to find module '#{source.uri}' on https://github.com"
+            end
+
+            all_versions = data.map { |r| r['name'].gsub(/^v/, '') }.sort.reverse
+
+            all_versions.delete_if do |version|
+              version !~ /\A\d\.\d(\.\d.*)?\z/
+            end
+
+            @versions = all_versions.compact
+            debug { "  Module #{name} found versions: #{@versions.join(", ")}" }
+            @versions
+          end
+
+          def manifests
+            versions.map do |version|
+              Manifest.new(source, name, version)
+            end
+          end
+
+          def install_version!(version, install_path)
+            if environment.local? && !vendored?(source.uri, version)
+              raise Error, "Could not find a local copy of #{source.uri} at #{version}."
+            end
+
+            vendor_cache(source.uri, version) unless vendored?(source.uri, version)
+
+            cache_version_unpacked! version
+
+            if install_path.exist?
+              install_path.rmtree
+            end
+
+            unpacked_path = version_unpacked_cache_path(version).children.first
+            cp_r(unpacked_path, install_path)
+          end
+
+          def environment
+            source.environment
+          end
+
+          def cache_path
+            @cache_path ||= source.cache_path.join(name)
+          end
+
+          def version_unpacked_cache_path(version)
+            cache_path.join('version').join(hexdigest(version.to_s))
+          end
+
+          def hexdigest(value)
+            Digest::MD5.hexdigest(value)
+          end
+
+          def cache_version_unpacked!(version)
+            path = version_unpacked_cache_path(version)
+            return if path.directory?
+
+            path.mkpath
+
+            target = vendored?(source.uri, version) ? vendored_path(source.uri, version) : name
+
+            Librarian::Posix.run!(%W{tar xzf #{target} -C #{path}})
+          end
+
+          def vendored?(name, version)
+            vendored_path(name, version).exist?
+          end
+
+          def vendored_path(name, version)
+            environment.vendor_cache.mkpath
+            environment.vendor_cache.join("#{name.sub("/", "-")}-#{version}.tar.gz")
+          end
+
+          def vendor_cache(name, version)
+            clean_up_old_cached_versions(name)
+
+            url = "https://api.github.com/repos/#{name}/tarball/#{version}"
+            url << "?access_token=#{ENV['GITHUB_API_TOKEN']}" if ENV['GITHUB_API_TOKEN']
+
+            File.open(vendored_path(name, version).to_s, 'wb') do |f|
+              begin
+                debug { "Downloading <#{url}> to <#{f.path}>" }
+                open(url,
+                  "User-Agent" => "librarian-puppet v#{Librarian::Puppet::VERSION}") do |res|
+                  while buffer = res.read(8192)
+                    f.write(buffer)
+                  end
+                end
+              rescue OpenURI::HTTPError => e
+                raise e, "Error requesting <#{url}>: #{e.to_s}"
+              end
+            end
+          end
+
+          def clean_up_old_cached_versions(name)
+            Dir["#{environment.vendor_cache}/#{name.sub('/', '-')}*.tar.gz"].each do |old_version|
+              FileUtils.rm old_version
+            end
+          end
+
+        private
+
+          def api_call(path)
+            tags = []
+            url = "https://api.github.com#{path}?page=1&per_page=100"
+            while true do
+              debug { "  Module #{name} getting tags at: #{url}" }
+              url << "&access_token=#{ENV[TOKEN_KEY]}" if ENV[TOKEN_KEY]
+              response = http_get(url, :headers => {
+                "User-Agent" => "librarian-puppet v#{Librarian::Puppet::VERSION}"
+              })
+
+              code, data = response.code.to_i, response.body
+
+              if code == 200
+                tags.concat JSON.parse(data)
+              else
+                begin
+                  message = JSON.parse(data)['message']
+                  if code == 403 && message && message.include?('API rate limit exceeded')
+                    raise Error, message + " -- increase limit by authenticating via #{TOKEN_KEY}=your-token"
+                  elsif message
+                    raise Error, "Error fetching #{url}: [#{code}] #{message}"
+                  end
+                rescue JSON::ParserError
+                  # response does not return json
+                end
+                raise Error, "Error fetching #{url}: [#{code}] #{response.body}"
+              end
+
+              # next page
+              break if response["link"].nil?
+              next_link = response["link"].split(",").select{|l| l.match /rel=.*next.*/}
+              break if next_link.empty?
+              url = next_link.first.match(/<(.*)>/)[1]
+            end
+            return tags
+          end
+
+          def http_get(url, options)
+            uri = URI.parse(url)
+            http = Net::HTTP.new(uri.host, uri.port)
+            http.use_ssl = true
+            request = Net::HTTP::Get.new(uri.request_uri)
+            options[:headers].each { |k, v| request.add_field k, v }
+            http.request(request)
+          end
+        end
+      end
+    end
+  end
+end
